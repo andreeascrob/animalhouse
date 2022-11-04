ead82705c7ffac64a5dbf8866a9c08a0e3af22e5
Luca Bassi <luca@argoware.com>
2022-11-04 19:05:40 +0100

    Fix ProductCard name link

--- server/static/front/components/ProductCard.js
+++ server/static/front/components/ProductCard.js
@@ -8,7 +8,7 @@ export class ProductCard extends HTMLElement {
 	<a href="/front/product/${this.getAttribute('slug')}"><img class="w-full aspect-square rounded-t-xl bg-neutral-300 dark:bg-neutral-800" src="${this.getAttribute('image') ?? ''}" loading="lazy" alt=""></a>
 	<div class="flex flex-row p-4 gap-2 grow">
 		<div class="flex flex-col justify-center grow">
-			<a href="${this.getAttribute('url')}"><p class="font-bold font-lg">${this.getAttribute('productname')}</p></a>
+			<a href="/front/product/${this.getAttribute('slug')}"><p class="font-bold font-lg">${this.getAttribute('productname')}</p></a>
 			<p>${window.eurIT.format(this.getAttribute('price'))}</p>
 		</div>
 		<button class="bg-emerald-500 dark:bg-emerald-800 p-4 rounded-full self-center shrink-0" onclick="addToCart('${this.getAttribute('slug')}', 1)"><img class="dark:invert" width="20" src="/icons/cart-plus.svg"></button>
